90bde09e56e8c1090a185f2dad327b45786b8e0f
Bill Moritz <billmoritz@gmail.com>
2012-12-15 12:52:08 -0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Envtracker::Application.config.secret_token = '481e7e90f332601b9fc301cdd3adef9de9e01b6d56a6e002e7a733260d0540506bb02cb8792212ab039993b2fd5d8167e75779ee026742f3fa81671f6b6021d5'
