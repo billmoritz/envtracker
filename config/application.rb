5a6641feeb58a1ce6d548625bb5c79f1e3286207
Bill Moritz <billmoritz@gmail.com>
2013-01-06 17:43:28 -0500

    Faster asset precompiles

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Envtracker
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # For faster asset precompiles
+    config.assets.initialize_on_precompile = false
   end
 end
